dd3fd3bfd91daba7937118e4a65d0ae232eab92b
Tedu <1053578154@qq.com>
2019-02-20 12:10:44 +0800

    second commit

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 #_*_coding_*_='utf-8'
 #服务端
 '''
-udp协议
+tcp传输协议
 处理客户端发送来的请求进行处理
 多进程
 
@@ -24,57 +24,87 @@ ADDR = (HOST,PORT)
 def main():
     #创建数据库链接
     db = pymysql.connect('localhost','root','123456','project')
-
+    
     #创建套接字
-    s = socket(AF_INET,SOCK_DGRAM)
+    s = socket()
+    s.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
     #绑定地址
     s.bind(ADDR)
+    s.listen(3)
     #处理僵尸进程
     signal.signal(signal.SIGCHLD,signal.SIG_IGN)
 
+    print("connect form port 8888")
     while True:
-        data,addr = s.recvfrom(1024)
+        try:
+            c,addr = s.accept()
+            print("Connect from",addr)
+        except KeyboardInterrupt:
+            s.close()
+            sys.exit("服务器退出")
+        except Exception as e:
+            print(e)
+            continue 
+        
         #创建子进程
         pid = os.fork()
         if pid == 0:
             s.close()
-            do_child(s,db)
+            do_child(c,db) #子进程函数
             sys.exit(0)
         else:
-            s.close()
+            c.close()
 
 #处理子进程
-def do_child(s,db):
+def do_child(c,db):
     while True:
-        data = s.recvfrom(128).decode()
+        data = c.recv(128)
         if data[0] == 'L':
-            do_login(s,db,data)
+            do_login(c,db,data)
         elif data[0] == 'R':
-            do_register(s,db,data)
+            do_register(c,db,data)
         elif data[0] == 'S':
-            do_search(s,db,data)
+            do_search(c,db,data)
         
-def do_register(s,db,data):
+def do_register(c,db,data):
     name = data.split(' ')[1]
     password = data.split(' ')[2]
-    #创建游标和sql语句
+
     cursor = db.cursor()
-    sql = 'select keyword from 关键词 = %s'%name
-    #执行sql语句
+    #创建游标和sql语句
+    sql = "select * from user where username = '%s'"%name
     cursor.execute(sql)
     r = cursor.fetchone()
+    
     #如果查找的r不为空，返回一个exist，如果为空，则插入语句
     if r != None:
-        s.sendto(b'exist')
+        c.send(b'EXIST')
         return
+
     sql = "insert into user(username,password) values('%s','%s')"%(name,password)
     try:
         #执行sql语句
         cursor.execute(sql)
         db.commit()
-        s.sendto(b'OK')
+        c.send(b'OK')
     except Exception:
         db.rollback()
-        s.send(b'False')
+        c.send(b'False')
+def do_login(c,db,data):
+    while True:
+        name = data.split(' ')[1]
+        password = data.split(' ')[2]
+
+        #创建游标对象
+        cursor = db.cursor()
+        #编写sql语句
+        sql = "select * from user where username = '%s'"%name
+        r = cursor.fetchone()
+
+        if r != None:
+            c.send(b'OK')
+        else:
+            c.send(b'fault')
+
 
 main()
